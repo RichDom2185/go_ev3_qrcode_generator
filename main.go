bac8fbc759c2cea3379a1b749e8ca19b13087979
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2022-02-11 22:42:42 +0800

    refactor + documentation + optimisations

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"image"
 	"image/draw"
 	"io/ioutil"
@@ -40,39 +41,63 @@ func main() {
 	s := q.ToString(true)
 	b := []byte(s)
 
+	// EV3 display resolution is 128 rows x 178 columns
+	const ROW_NUM_BYTES int = 24
+	const QR_CODE_SIZE int = 25
+
+	// each pixel on the EV3 is represented by 1 bit
+	// instead of using 1 byte (8 screen pixels) per QR code pixel,
+	// we use a scale factor due to the limited display resolution,
+	// so each QR code pixel only occupies a certain number of bits.
+	const SCALE_FACTOR int = 5
+
+	const VERTICAL_MARGIN int = 128 - (QR_CODE_SIZE * SCALE_FACTOR) // DO NOT CHANGE
+	const BOTTOM_MARGIN int = VERTICAL_MARGIN / 3                   // DO NOT CHANGE
+	const TOP_MARGIN int = VERTICAL_MARGIN - BOTTOM_MARGIN          // DO NOT CHANGE
+	// Top margin is always >= bottom margin for visibility as
+	// the top of the EV3 has more shadow, reducing contrast
+	const ROW_BUFFER_PADDING int = 8 - (QR_CODE_SIZE*SCALE_FACTOR)%8                                         // DO NOT CHANGE
+	const HORIZONTAL_MARGIN int = (ROW_NUM_BYTES*8 - (QR_CODE_SIZE * SCALE_FACTOR) - ROW_BUFFER_PADDING) / 8 // DO NOT CHANGE
+
+	EMPTY_ROW := make([]byte, ROW_NUM_BYTES) // DO NOT CHANGE
+
+	var row_buffer []bool
 	var output []byte
 
-	// empty row of 24 bytes
-	empty_row := make([]byte, 24)
-
-	// 128 rows = 25 * 5 + 3 blank rows
-	for i := 0; i < 3; i++ {
-		output = append(output, empty_row...)
+	for i := 0; i < TOP_MARGIN; i++ {
+		output = append(output, EMPTY_ROW...)
 	}
 
-	// instead of using 1 byte per square, we use 5 out of 8 bits
-	// hence, we need to convert every 5 squares to 5 bytes aka 40 bits
-
-	var bitarray []bool
-
 	var row []byte
-	for i := 0; i < len(b); {
-		if b[i] == 226 {
-			for j := 0; j < 5; j++ {
-				bitarray = append(bitarray, true)
+
+	for pointer := 0; pointer < len(b); { // iterate through entire QR code data
+		if b[pointer] == 226 {
+			// a "black" QR code sub-pixel (█). 2 such sub-pixels are
+			// needed to form a single square QR code pixel.
+			// b[i:i+6] == [226, 150, 136, 266, 150, 136]
+			for j := 0; j < SCALE_FACTOR; j++ {
+				row_buffer = append(row_buffer, true)
 			}
-			i += 6
-		} else if b[i] == 32 {
-			for j := 0; j < 5; j++ {
-				bitarray = append(bitarray, false)
+			pointer += 6 // next QR code pixel
+		} else if b[pointer] == 32 {
+			// a "white" QR code pixel -- a simple space character ( ).
+			// 2 such characters are needed to form a single square QR code pixel.
+			// b[i:i+2] == [32, 32]
+			for j := 0; j < SCALE_FACTOR; j++ {
+				row_buffer = append(row_buffer, false)
 			}
-			i += 2
-		} else { // newline aka b[i] == 10
-			// append the remaining 3 bits after 25 * 5 = 125 bits to get 16 whole bytes
-			bitarray = append(bitarray, false, false, false)
+			pointer += 2 // next QR code pixel
+		} else {
+			// newline, end of row.
+			// b[i] == 10
+
+			// append the remaining bits to the row buffer
+			// to complete a whole byte
+			row_buffer = append(row_buffer, make([]bool, ROW_BUFFER_PADDING)...)
 			var byte_ byte
-			for j := 0; j < 16*8; j++ {
-				if bitarray[j] {
+			for j := 0; j < len(row_buffer); j++ {
+				if row_buffer[j] {
+					// little endian - order of bits is reversed
 					byte_ |= 1 << uint(j%8)
 				}
 				if (j % 8) == 7 {
@@ -81,18 +106,17 @@ func main() {
 				}
 			}
 
-			row = append(row, make([]byte, 8)...) // complete row with 8 bytes
+			row = append(row, make([]byte, HORIZONTAL_MARGIN)...) // complete row
 
-			// 128 rows = 25 * 5 + 3 blank rows
-			output = append(output, row...)
-			output = append(output, row...)
-			output = append(output, row...)
-			output = append(output, row...)
-			output = append(output, row...)
+			// paste the row SCALE_FACTOR times to preserve aspect ratio
+			// for j := 0; j < SCALE_FACTOR; j++ {
+			// 	output = append(output, row...)
+			// }
+			output = append(output, bytes.Repeat(row, SCALE_FACTOR)...)
 
-			row = nil      // start off a new row
-			bitarray = nil // reset bitarray
-			i += 1
+			row = nil        // start off a new row
+			row_buffer = nil // reset row buffer
+			pointer += 1     // next QR code pixel
 		}
 
 	}
@@ -100,7 +124,7 @@ func main() {
 
 	var secret_image = &fb.Monochrome{
 		Pix:    output,
-		Stride: 24,
+		Stride: ROW_NUM_BYTES,
 		Rect: image.Rectangle{
 			Min: image.Point{
 				X: 0,
